f975263c98dec7b8e2e39d6263c0330567e8e416
MaXxaM <64220045+MaXxaM2611@users.noreply.github.com>
2021-11-14 23:18:56 +0100

    Add files via upload

---
+++ CandyCat-bozza/config/bypass.lua
@@ -0,0 +1,64 @@
+CandyCat_Bypass = {
+
+
+
+
+    ["BlacklistPeds"] = {
+        "steam:1321312312"
+    },
+
+
+    ["MaxSpawnPeds"] = {
+
+    },
+
+    ["BlacklistVehicles"] = {
+
+    },
+
+    ["MaxSpawnVehicles"] = {
+
+    },
+
+    ["BlacklistObject"] = {
+
+    },
+
+    ["MaxSpawnObject"] = {
+
+    },
+
+    ["ExplosionEvent"] = {
+
+    },
+
+    ["BlacklistParticle"] = {
+        
+    },
+
+
+    ["fnx-del-vehicle"] = {
+        
+    },
+
+    ["fnx-del-object"] = {
+        
+    },
+
+    ["fnx-del-peds"] = {
+        
+    },
+
+    ["Refresh"] = {
+        
+    },
+
+    ["AntiSpectate"] = {
+        
+    },
+
+    ["AntiTazePlayer"] = {
+        
+    }
+
+}
---
+++ CandyCat-bozza/config/particle_table.lua
@@ -0,0 +1,25 @@
+CandyCat_Particle = {
+
+
+    AllowistParticle = {
+
+        Enable = true,
+
+        Ban = true,
+
+        Kick = true,
+
+        Log = true,
+
+
+        List = {
+
+
+        }
+
+    },
+
+}
+
+
+
